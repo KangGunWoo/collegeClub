3ad97b46aab999afc251e0f566289dbc0c80740c
김도영 <43737757+kd2675@users.noreply.github.com>
2022-05-16 16:30:36 +0900

    220516v1.0

--- react-flask-kimd0/flask/Flask.py
+++ react-flask-kimd0/flask/Flask.py
@@ -2,17 +2,28 @@ from flask import Flask, render_template
 from flask import request, redirect
 from flask_cors import CORS
 
-app = Flask(__name__)
+import pymysql
+import json
 
+db = mymysql.connect(host='localhost', port=3306, user='root', passwd='1234',
+                     db='test
+
+json_string = {
+    "id": "hello world"
+}
+json_string = json.dumps(json_string)
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
 CORS(app)
 
 @app.route('/', methods=['GET','POST'])
 def index():
     
-    return render_template("/index.html")
+    return json_string
 
 
 if __name__ == '__main__':
-    app.run(host = "0.0.0.0", port = "8083")
+    app.run(host = "0.0.0.0", port = "5000", debug = True)
 
 
--- react-flask-kimd0/react-app/src/App.js
+++ react-flask-kimd0/react-app/src/App.js
@@ -1,23 +1,97 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import axios from 'axios';
 import './App.css';
+import logoImage1 from './images/logo.jpg'
+import menubarImage01 from './images/menubar01.jpg'
+import menubarImage02 from './images/menubar02.jpg'
+import menubarImage03 from './images/menubar03.jpg'
+import menubarImage04 from './images/menubar04.jpg'
+import menubarImage05 from './images/menubar05.jpg'
+
+
+
+
+function Logo(){
+  return(
+    <div className='logo'>
+      <img src={logoImage1} alt='이미지가 없습니다.'></img>
+    </div>
+  )
+}
+
+function Main(){
+  return(
+    <div className='main'>
+      
+    </div>
+  )
+}
+
+function Select(){
+  return(
+    <div className='select'>
+      <div className='selectButton1'>
+        <SelectButton></SelectButton>
+      </div>
+      <div className='selectButton2'>
+        <SelectButton></SelectButton>
+      </div>
+    </div>
+  )
+}
+
+function SelectButton(){
+  return(
+    <div className='selectButton'>
+
+    </div>
+  )
+}
+
+function Menubar(){
+  const a = 
+    [menubarImage01,
+      menubarImage02,
+      menubarImage03,
+      menubarImage04,
+      menubarImage05];
+    
+
+  return(
+    <div className='menubar'>
+      {a.map((i, index) => {
+        return (
+          <div key={index} className='menubarDiv'>
+            <img src={i} alt='이미지없음'></img>
+          </div>
+        )
+      })}
+    </div>
+  )
+}
+
+function App1(){
+  
+  return (
+    <button className="btn btn-primary" onClick={()=>{
+      
+      axios.get('http://localhost:5000')
+      .then((respose)=>{ console.log(respose.data.id) })
+      
+
+    }}>더보기</button>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Logo></Logo>
+      <Main></Main>
+      <Select></Select>
+      <Menubar></Menubar>
+      <App1>
+      </App1>
     </div>
   );
 }
