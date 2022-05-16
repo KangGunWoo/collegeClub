fc45f24d79298d6b37075287f91bf7579942aac7
강건우 <98583833+KangGunWoo@users.noreply.github.com>
2022-05-16 16:53:55 +0900

    220516v1.0

--- 강건우/react-app/src/App.js
+++ 강건우/react-app/src/App.js
@@ -1,23 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="wrap">
+        <div className="header">
+          <div className="campus"><div className='incam'></div></div>
+          <div className='screen1'><div className="screen2"></div>
+            전개화면
+          </div>
+        </div>
+        <div className="sec1"></div>
+        <div class="sec2"></div>
+        <div className="footer">
+          <div className="levelsec">
+            <div className="pre">이전</div>
+            <div className="level">
+              <p className="que">Q3</p>
+              <p className="que">○ ● ○ ○ ○ ○ ○ ○</p>
+            </div>
+            <div className="next">다음</div>
+
+
+          </div>
+          <div class="footerImg"></div>
+        </div>
+
+      </div>
     </div>
   );
 }
